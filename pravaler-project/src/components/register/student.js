b90a7a87acdd5a8994a16b2994d59711a3acbe74
jaquew <eng.jaquesilva@gmail.com>
2020-02-11 17:58:07 -0300

    validade phone and cpf

--- pravaler-project/src/components/register/student.js
+++ pravaler-project/src/components/register/student.js
@@ -1,40 +1,60 @@
 import React, {useState} from "react";
 import Input from '../../components/input/input';
+import { validate as validateCPF } from 'gerador-validador-cpf'
 
 const StudentData = () => {
   const [cpfNumber, setCpfNumber]=useState('')
+  const [phoneNumber, setPhoneNumber]=useState('')
 
-    return (
-      <form>
-        <Input 
-          type={'number'} 
-          placeholder={'cpf'}
-          onChange={(e) => cpfValidate(e)}
-        />
-      </form>
-    );
+  const cpfValidate = (e) => {
+    let cpfValue= (e.currentTarget.value)
+    setCpfNumber(cpfValue)
+    console.log(cpfValue);
 
-  function cpfValidate(e){
-  const regex = /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/gm
-  const cpfValue= (e.currentTarget.value)
-  setCpfNumber(cpfValue)
-  console.log(typeof(cpfValue));
-  
-  console.log(cpfValue.match(regex));
-  console.log(regex.exec(cpfValue))
-
-  
-
+    const cpfValido = /^(([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}))$/;  if (cpfValido.test(cpfValue) === false) {
+      cpfValue = cpfValue.replace( /\D/g , "");
+      cpfValue = cpfValue.replace( /(\d{3})(\d)/ , "$1.$2"); 
+      cpfValue = cpfValue.replace( /(\d{3})(\d)/ , "$1.$2");
+      cpfValue = cpfValue.replace( /(\d{3})(\d{1,2})$/ , "$1-$2");
+      setCpfNumber(cpfValue)    
     }
+    console.log(validateCPF(cpfValue))
+  }
 
+  const phoneValidate = (e) => {
+    let phoneValue= (e.currentTarget.value)
+      const validPhone = /^((([0-9]{2})[0-9]{3}.[0-9]{3}-[0-9]{2}))$/; 
+      // const number = phoneValue.match(/\d/g, "");
+      // console.log(number);
+      if (validPhone.test(phoneValue) === false) {
+        phoneValue = phoneValue.replace( /\D/g , "");
+        phoneValue = phoneValue.replace( /(\d{0})(\d)/ , "$1($2"); 
+        phoneValue = phoneValue.replace( /(\d{2})(\d)/ , "$1) $2"); 
+        phoneValue = phoneValue.replace( /(\d{5})(\d)/ , "$1-$2");
+        setPhoneNumber(phoneValue)    
+      }
+  }
+
+  return (
+    <form>
+      <Input 
+        type={'text'} 
+        placeholder={'cpf'}
+        value={cpfNumber}
+        maxlength={14}
+        onChange={cpfValidate}
+      />
+         <Input 
+        type={'text'} 
+        placeholder={'telefone'}
+        value={phoneNumber}
+        maxlength={15}
+        onChange={phoneValidate}
+      />
+    </form>
+  );
 };
 
+
 export default StudentData
-
-// telefone celular
-//^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$
-
-// renda mensal
-// ^[1-9]\d{0,2}(\.\d{3})*,\d{2}$
-
     
